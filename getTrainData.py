70daabd53935af11b9843820cb0e15013b4490a4
01miaom <63391992+01miaom@users.noreply.github.com>
2022-07-06 12:54:46 +0800

    Update getTrainData.py

--- getTrainData.py
+++ getTrainData.py
@@ -1,11 +1,18 @@
 import requests
 import bs4
 
-url=""
-response = requests.get(url)
+count = 20
+searchName = 'cylinder'
+url="https://cn.bing.com/images/async?q="+searchName+"&first=12&count="+str(count)+"&cw=1177&ch=729&relp=35&tsc=ImageHoverTitle&datsrc=I&layout=RowBased&mmasync=1"
+headers = {
+    'cookie': '', #fill with your cookie
+    'user-agent': '' #fill with your user-agent
+}
+
+response = requests.get(url, headers=headers)
 data=bs4.BeautifulSoup(response.content, "html.parser")
 element = data.find_all("img", class_="mimg")
-for i in range(1,100):
+for i in range(1,count):
     response = requests.get(element[i].get('src'))
     fileName = str(i)+'.jpeg'
     with open(fileName.format(0),'wb') as f:
